ed17b9604b6ffe1150647da32a6325d4058b267c
Matt Hoiland <matthew.hoiland@vivint.com>
2019-04-18 14:03:11 -0600

    Add utility script
    Add skbk.py for programmatically creating, managing, and deleting
    sketches.

---
+++ skbk.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+skbk.py: a utility module for managing sketches
+"""
+
+# Built-in imports
+import sys
+
+# 3rd party imports
+
+# Path changes
+
+# My modules
+
+# Authorship data
+
+__author__      = "Matt Hoiland"
+__copyright__   = "Copyright 2019, Matt Hoiland"
+__license__     = "GPL"
+__version__     = "0.1"
+__maintainer__  = "Matt Hoiland"
+__email__       = "matthew.proctor.hoiland@gmail.com"
+__status__      = "Prototype"
+
+################################################################################
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
