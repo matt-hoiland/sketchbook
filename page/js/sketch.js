1d9b8ed57d9a96ed1bfe1b6dce8e216c39d47ab4
Matt Hoiland <matthew.hoiland@vivint.com>
2019-04-18 12:42:04 -0600

    Remove sketch 'page'
    Proof of concept for embedding p5.js sketches into other webpages. I
    will revisit this idea in the future, but this sketch serves no purposes
    for it. Begone!

--- page/js/sketch.js
+++
@@ -1,9 +0,0 @@
-const sketch = new p5((p) => {
-  p.setup = function() {
-    let container = p.select('#canvas-container')
-    console.log(container)
-    let cnv = p.createCanvas(container.width, container.height)
-    cnv.parent(container)
-    p.background('pink')
-  }
-})
