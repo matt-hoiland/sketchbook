112a197b5b31d1ecf3195c9895f2539d2bffa9d6
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-11 15:56:27 -0600

    Clean 'darktree'

--- sketches/darktree/js/sketch.js
+++ sketches/darktree/js/sketch.js
@@ -1,84 +1,80 @@
-var tree
+let tree;
 
-var s = new p5((p) => {
-  var canvas
-  var interval
+function initSketch(p) {
+  let canvas;
+  let interval;
 
   p.genTree =  function generateTree(n) {
-      var t = new Tree(p.random(-p.QUARTER_PI, p.QUARTER_PI), p.random(1,8) * n)
+      let t = new Tree(p.random(-p.QUARTER_PI, p.QUARTER_PI), p.random(1,8) * n);
       if (n == 0) {
-        return t
+        return t;
       }
-      for (var i = p.floor(p.random(1,4)); i > 0; i--) {
-        t.add(generateTree(n - 1))
+      for (let i = p.floor(p.random(1,4)); i > 0; i--) {
+        t.add(generateTree(n - 1));
       }
-      return t
+      return t;
   }
 
   p.setup = () => {
-    canvas = p.createCanvas(p.windowWidth, p.windowHeight * 3 / 4)
-    tree = p.genTree(12)
-    tree.a = 0
+    canvas = p.createCanvas(CANVAS_SIZE.DEFAULT, CANVAS_SIZE.DEFAULT);
+    tree = p.genTree(12);
+    tree.a = 0;
     interval = setInterval(() => {
-      tree = p.genTree(12)
-      tree.a = 0
-    }, 5000)
+      tree = p.genTree(12);
+      tree.a = 0;
+    }, 5000);
   }
 
   p.draw = () => {
-    p.background(42)
-    p.fill(0)
-    p.noStroke()
-    p.translate(p.width/2, p.height)
-    tree.draw(p)
+    p.background(42);
+    p.fill(0);
+    p.noStroke();
+    p.translate(p.width/2, p.height);
+    tree.draw(p);
   }
+}
 
-  p.windowResized = () => {
-    canvas = p.resizeCanvas(p.windowWidth, p.windowHeight * 3 / 4);
-  }
-})
-
-var factor = 150
+let factor = 150;
 
 function Tree(a, l) {
-  this.a = a
-  this.l = l
-  this.h = undefined
-  this.cs = []
+  this.a = a;
+  this.l = l;
+  this.h = undefined;
+  this.cs = [];
 
   this.height = () => {
     if (this.h !== undefined) {
-      return this.h
+      return this.h;
     }
     if (this.cs.length == 0) {
-      this.h = 1
+      this.h = 1;
     } else {
       this.h = this.cs.reduce((a, b) => {
-        return Math.max(a, b.height())
-      }, 0)
+        return Math.max(a, b.height());
+      }, 0);
     }
-    return this.h
+    return this.h;
   }
 
   this.add = (t) => {
-    this.cs.push(t)
+    this.cs.push(t);
     if (this.height() <= t.height()) {
-      this.h = t.h + 1
+      this.h = t.h + 1;
     }
   }
 
   this.draw = (p) => {
-    p.push()
-    p.rotate(this.a)
-    var w = this.height() * this.height() * this.height() / factor;
-    var h = this.l
-    p.rect(-w/2, 0, w, -h)
-    // p.stroke(p.color("red"))
-    // p.line(0,0,0,-h + 10)
-    p.translate(0,-h)
+    p.push();
+    p.rotate(this.a);
+    let w = this.height() * this.height() * this.height() / factor;
+    let h = this.l;
+    p.rect(-w/2, 0, w, -h);
+    // p.stroke(p.color("red"));
+    // p.line(0,0,0,-h + 10);
+    p.translate(0,-h);
     this.cs.forEach((v) => {
       v.draw(p);
-    })
-    p.pop()
+    });
+    p.pop();
   }
 }
