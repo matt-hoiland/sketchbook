0572d21a0130a9a08e94d2736dacdeb492e53b0f
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-11 15:34:45 -0600

    Clean 'blocks'

--- sketches/blocks/js/block.js
+++ sketches/blocks/js/block.js
@@ -1,65 +1,65 @@
 function Block(orgn, phi, l, clr) {
-  this.orgn = orgn
-  this.phi = phi
-  this.theta = 0
-  this.l = l
-  this.clr = clr
-  this.squished = false
-  this.t = -1
-  this.g = -4
-  this.v = 0
-  this.y = 0
+  this.orgn = orgn;
+  this.phi = phi;
+  this.theta = 0;
+  this.l = l;
+  this.clr = clr;
+  this.squished = false;
+  this.t = -1;
+  this.g = -4;
+  this.v = 0;
+  this.y = 0;
   this.jumping = false;
 
-  var max_theta = 10
-  var dep_rate = 1
-  var max_v = 40
+  let max_theta = 10;
+  let dep_rate = 1;
+  let max_v = 40;
 
   this.draw = function(p) {
-    p.push()
-    // p.colorMode(HSB)
-    p.fill(clr)
-    p.translate(this.orgn.x, this.orgn.y - this.y)
-    var a = p.radians(this.phi - this.theta)
-    var x = this.l * p.cos(a)
-    var y = this.l * p.sin(a)
-    p.rect(-x/2, -y, x, y)
-    p.pop()
+    p.push();
+    // p.colorMode(HSB);
+    p.fill(clr);
+    p.translate(this.orgn.x, this.orgn.y - this.y);
+    let a = p.radians(this.phi - this.theta);
+    let x = this.l * p.cos(a);
+    let y = this.l * p.sin(a);
+    p.rect(-x/2, -y, x, y);
+    p.pop();
 
-    this.update(p)
+    this.update(p);
   }
 
   this.update = function(p) {
     if (this.squished) {
-      this.theta += (this.theta >= max_theta ? 0 : dep_rate)
-    } else if (this.t >= 0 && this.t < 20){
-      this.theta = - max_theta * p.cos(p.radians(this.t)) / (1 + this.t)
-      this.t += dep_rate
+      this.theta += (this.theta >= max_theta ? 0 : dep_rate);
+    } else if (this.t >= 0 && this.t < 20) {
+      this.theta = - max_theta * p.cos(p.radians(this.t)) / (1 + this.t);
+      this.t += dep_rate;
     } else {
-      this.t = -1
+      this.t = -1;
     }
     if (this.jumping) {
-      this.v += this.g
-      this.y += this.v
+      this.v += this.g;
+      this.y += this.v;
       if (this.y < 0) {
-        this.y = 0
-        this.jumping = false
+        this.y = 0;
+        this.jumping = false;
         console.log("landed!")
-        this.theta = max_theta
-        this.t = 0
+        this.theta = max_theta;
+        this.t = 0;
       }
     }
   }
 
   this.press = function() {
-    this.squished = true
+    this.squished = true;
   }
 
   this.release = function() {
-    this.squished = false
-    this.t = 0
-    this.v = max_v
+    this.squished = false;
+    this.t = 0;
+    this.v = max_v;
     this.jumping = true;
-    console.log("jump!")
+    console.log("jump!");
   }
 }
