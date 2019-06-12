7e24aa95dd6ee1707ab90028e6b375d178822f9b
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-12 13:49:47 -0600

    Clean 'tetris'

--- sketches/tetris/js/sketch.js
+++ sketches/tetris/js/sketch.js
@@ -6,48 +6,50 @@ let colors = {
   'S': 'green',
   'T': 'purple',
   'Z': 'red'
-}
+};
 
 class Mino {
   constructor() {
-    this.position
+    this.position;
   }
 }
 
 class Board {
   constructor() {
-    this.width = 10
-    this.height = 22
-    this.board = Array(this.height).fill().map(() => Array(this.width).fill(null))
+    this.width = 10;
+    this.height = 22;
+    this.board = Array(this.height).fill().map(() => Array(this.width).fill(null));
     this.cell_size = 20;
   }
 
-  draw() {
-    push()
-    translate(width/2, height/2)
-    translate(this.width / 2 * - this.cell_size, (this.height - 2) / 2 * - this.cell_size)
+  draw(p) {
+    p.push();
+    p.translate(p.width/2, p.height/2);
+    p.translate(this.width / 2 * - this.cell_size, (this.height - 2) / 2 * - this.cell_size);
 
     for (let i = 2; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        push()
-        translate(this.cell_size * j, this.cell_size * (i - 2))
-        stroke(150)
-        if (this.board[i][j] !== null) { fill(color(this.board[i][j])) }
-        else { noFill() }
-        rect(0, 0, this.cell_size, this.cell_size)
-        pop()
+        p.push();
+        p.translate(this.cell_size * j, this.cell_size * (i - 2));
+        p.stroke(150);
+        if (this.board[i][j] !== null) { p.fill(p.color(this.board[i][j])); }
+        else { p.noFill(); }
+        p.rect(0, 0, this.cell_size, this.cell_size);
+        p.pop();
       }
     }
-    pop()
+    p.pop();
   }
 }
 
-let board
+let board;
 
-function setup() {
-  createCanvas(512, 512);
-  background(0)
-  board = new Board()
+function initSketch(p) {
+  p.setup = function() {
+    p.createCanvas(CANVAS_SIZE.DEFAULT, CANVAS_SIZE.DEFAULT);
+    p.background(0);
+    board = new Board();
 
-  board.draw()
+    board.draw(p);
+  }
 }
