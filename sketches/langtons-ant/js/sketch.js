c9a0b764a5cac5ef689a2c104bffca096092e198
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-12 12:55:14 -0600

    Clean 'langtons-ant'

--- sketches/langtons-ant/js/sketch.js
+++ sketches/langtons-ant/js/sketch.js
@@ -1,59 +1,59 @@
-let rate = 100
-const gridsize = 750
-const canvassize = 750
+function initSketch(p) {
+  let rate = 100;
+  const gridsize = 256;
+  const canvassize = CANVAS_SIZE.DEFAULT;
 
-let dnaString = 'lrrrrlllrrr'
-let dna = []
+  let dnaString = 'lrrrrlllrrr';
+  let dna = [];
 
-let colors = [
-  'red',
-  'orange',
-  'yellow',
-  'lime',
-  'green',
-  'skyblue',
-  'blue',
-  'purple',
-  'pink',
-  'white',
-  'brown'
-]
+  let colors = [
+    'red',
+    'orange',
+    'yellow',
+    'lime',
+    'green',
+    'skyblue',
+    'blue',
+    'purple',
+    'pink',
+    'white',
+    'brown'
+  ];
 
-var sketch = new p5((p) => {
   for (let i = 0; i < dnaString.length; ++i) {
     switch (dnaString.charAt(i)) {
       case 'l': dna.push(+1); break;
       case 'r': dna.push(-1); break;
     }
   }
-  var canvas
-  var grid
-  var step
-  var ant
-  var dir
+  let canvas;
+  let grid;
+  let step;
+  let ant;
+  let dir;
 
   p.setup = () => {
-    canvas = p.createCanvas(canvassize,canvassize)
-    grid = Array(gridsize).fill(null).map(() => Array(gridsize).fill(0))
-    console.log(grid)
-    step = p.floor(canvassize/gridsize)
-    ant = p.createVector(gridsize/2, gridsize/2)
+    canvas = p.createCanvas(canvassize,canvassize);
+    grid = Array(gridsize).fill(null).map(() => Array(gridsize).fill(0));
+    console.log(grid);
+    step = p.floor(canvassize/gridsize);
+    ant = p.createVector(gridsize/2, gridsize/2);
     dir = 0;
-    p.background(0)
-    p.noStroke()
+    p.background(0);
+    p.noStroke();
   }
 
   p.draw = () => {
     for (let i = 0; i < rate; i++) {
       if (dna[grid[ant.x][ant.y]] > 0) {
-        dir = (dir + 1) % 4
+        dir = (dir + 1) % 4;
       } else {
-        dir = (dir - 1 < 0 ? 3 : dir - 1)
+        dir = (dir - 1 < 0 ? 3 : dir - 1);
       }
-      p.fill(colors[grid[ant.x][ant.y]])
-      p.rect(ant.x * step, ant.y * step, step, step)
-      grid[ant.x][ant.y]++
-      grid[ant.x][ant.y] %= dna.length
+      p.fill(colors[grid[ant.x][ant.y]]);
+      p.rect(ant.x * step, ant.y * step, step, step);
+      grid[ant.x][ant.y]++;
+      grid[ant.x][ant.y] %= dna.length;
       switch (dir) {
         case 0:
           ant.y++; break;
@@ -64,20 +64,20 @@ var sketch = new p5((p) => {
         case 3:
           ant.x--; break;
         default:
-          console.log("WRONG!!!! " + dir)
+          console.log("WRONG!!!! " + dir);
       }
       if (ant.y >= gridsize) {
-        ant.y = 0
+        ant.y = 0;
       }
       if (ant.y < 0) {
-        ant.y = gridsize - 1
+        ant.y = gridsize - 1;
       }
       if (ant.x >= gridsize) {
-        ant.x = 0
+        ant.x = 0;
       }
       if (ant.x < 0) {
-        ant.x = gridsize - 1
+        ant.x = gridsize - 1;
       }
     }
   }
-})
+}
