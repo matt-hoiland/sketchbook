25adb6a7f38c900f1836f4045672b27f5c69915d
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-12 13:53:19 -0600

    Clean 'trees'

--- sketches/trees/js/sketch.js
+++ sketches/trees/js/sketch.js
@@ -1,63 +1,64 @@
+function initSketch(p) {
+  let w = CANVAS_SIZE.DEFAULT;
+  let h = CANVAS_SIZE.DEFAULT;
 
-var w = 1200;
-var h = 600;
+  let root;
 
-var root;
+  let angle;
 
-var angle;
+  let slider;
 
-var slider;
-
-function setup() {
-  createCanvas(w, h);
-  // createSlider()
-  root = genTree(6,4);
-  angle = PI/5;
-  slider = createSlider(0, 180, 120);
-  slider.position(10,10);
-  slider.style('width', '360px')
-}
-
-function draw() {
-  var s = slider.value();
-  console.log(s);
-  push();
-  background(51);
-  stroke(0xffffff);
-  root.draw(w/2, h, radians(s), 200, 6);
-  pop();
-}
-
-function genTree(depth, bredth) {
-  if (depth === 0) { return null; }
-  var t = new Tree();
-  for (var i = 0; i < bredth; i++) {
-    var c = genTree(depth-1,bredth);
-    if (c != null) {
-      t.branches.push(c);
-    }
+  p.setup = function() {
+    p.createCanvas(w, h);
+    // createSlider()
+    root = genTree(6,4);
+    angle = p.PI/5;
+    slider = p.createSlider(0, 180, 120);
+    slider.position(10,10);
+    slider.style('width', '360px')
   }
-  return t;
-}
 
-function Tree() {
-  this.branches = []
+  p.draw = function() {
+    let s = slider.value();
+    console.log(s);
+    p.push();
+    p.background(51);
+    p.stroke(0xffffff);
+    root.draw(w/2, h, p.radians(s), 200, 6);
+    p.pop();
+  }
 
-  this.draw = function(x, y, angle, len, weight) {
-    push();
-    strokeWeight(weight);
-    translate(x,y);
-    line(0,0,0,-len);
-    translate(0,-len);
-    rotate(-PI/4);
-    var a = angle;
-    for (var i = 0; i < this.branches.length; i++) {
-      push();
-      rotate(a);
-      this.branches[i].draw(0,0, angle, 2*len/3, weight - 1);
-      pop();
-      a += angle;
+  function genTree(depth, bredth) {
+    if (depth === 0) { return null; }
+    let t = new Tree();
+    for (let i = 0; i < bredth; i++) {
+      let c = genTree(depth-1,bredth);
+      if (c != null) {
+        t.branches.push(c);
+      }
     }
-    pop();
+    return t;
+  }
+
+  function Tree() {
+    this.branches = []
+
+    this.draw = function(x, y, angle, len, weight) {
+      p.push();
+      p.strokeWeight(weight);
+      p.translate(x,y);
+      p.line(0,0,0,-len);
+      p.translate(0,-len);
+      p.rotate(-p.PI/4);
+      let a = angle;
+      for (let i = 0; i < this.branches.length; i++) {
+        p.push();
+        p.rotate(a);
+        this.branches[i].draw(0,0, angle, 2*len/3, weight - 1);
+        p.pop();
+        a += angle;
+      }
+      p.pop();
+    }
   }
 }
