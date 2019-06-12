5488bd8fd700ed503683d47c8f11ff1cf25dcab6
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-12 13:21:05 -0600

    Clean 'planets'

--- sketches/planets/js/sketch.js
+++ sketches/planets/js/sketch.js
@@ -1,26 +1,29 @@
-var sketch = new p5(function(p) {
-  var screen_ratio;
+function initSketch(p) {
+  let screen_ratio;
   p.screen_ratio = function(s) { if (s) screen_ratio = s; return s; }
 
-  var canvas;
+  let canvas;
   p.canvas = function() { return canvas; }
 
-  var earth;
-  var moon;
-  var mars;
+  let earth;
+  let moon;
+  let mars;
 
-  p.planets = function() { return [earth, moon]; }
+  p.planets = function() { return [earth, moon]; };
 
   p.setup = function() {
-    screen_ratio = {x:0.90, y:0.75}
-    canvas = p.createCanvas(p.windowWidth * screen_ratio.x, p.windowHeight * screen_ratio.y)
-    earth = new Planet(p.createVector(p.width * 0.25, p.height / 2), 200, p.createVector(0,4))
-    moon = new Planet(p.createVector(p.width * 0.75, p.height / 2), 50, p.createVector(0,-4))
+    canvas = p.createCanvas(CANVAS_SIZE.DEFAULT, CANVAS_SIZE.DEFAULT);
+    earth = new Planet(
+      p.createVector(p.width * 0.25, p.height / 2),
+      200,
+      p.createVector(0,4)
+    );
+    moon = new Planet(
+      p.createVector(p.width * 0.75, p.height / 2),
+      50,
+      p.createVector(0,-4)
+    );
     // mars = new Planet(p.createVector(p.width * 0.5, p.height * 0.25), 70, p.createVector(0,2))
-  }
-
-  p.windowResized = function() {
-    canvas = p.resizeCanvas(p.windowWidth * screen_ratio.x, p.windowHeight * screen_ratio.y)
   }
 
   p.draw = function() {
@@ -34,4 +37,4 @@ var sketch = new p5(function(p) {
     moon.update([earth, moon])
     // mars.update([earth, moon, mars])
   }
-}, 'game_box')
+}
