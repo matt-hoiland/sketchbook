6200971a4da9fb9d002d2b5c893e78bd86727b37
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-12 13:12:44 -0600

    Clean 'particles'

--- sketches/particles/js/sketch.js
+++ sketches/particles/js/sketch.js
@@ -1,36 +1,38 @@
-let a = []
+function initSketch(p) {
+  let a = [];
 
-function generatePoint() {
-  const min_theta = - radians(90 - 35);
-  const max_theta = - radians(90 + 35);
+  function generatePoint() {
+    const min_theta = - p.radians(90 - 35);
+    const max_theta = - p.radians(90 + 35);
 
-  let angle = random() * (max_theta - min_theta) + min_theta
-  let mag = (random(5) + 5) / 5
-  let [dx, dy] = [mag * cos(angle), mag * sin(angle)]
-  let [x, y] = [width * 0.5, height * 0.90]
+    let angle = p.random() * (max_theta - min_theta) + min_theta;
+    let mag = (p.random(5) + 5) / 5;
+    let [dx, dy] = [mag * p.cos(angle), mag * p.sin(angle)];
+    let [x, y] = [p.width * 0.5, p.height * 0.90];
 
-  return new Particle(x, y, dx, dy, (random(10) + 80) / 20000.0)
-}
-
-function setup() {
-  let cnv = createCanvas(500, 1000)
-  background(0)
-
-  a = Array(50).fill().map(generatePoint)
-}
-
-function draw() {
-  let deletes = []
-  background(0)
-  for (let p of a) {
-    p.draw()
-    p.next()
-    if (p.transparency === 0) {
-      deletes.push(p)
-    }
+    return new Particle(x, y, dx, dy, (p.random(10) + 80) / 20000.0);
   }
 
-  a = a.filter(p => !deletes.includes(p))
+  p.setup = function() {
+    let cnv = p.createCanvas(CANVAS_SIZE.DEFAULT, CANVAS_SIZE.DEFAULT);
+    p.background(0);
 
-  a.push(generatePoint())
+    a = Array(50).fill().map(generatePoint);
+  }
+
+  p.draw = function() {
+    let deletes = [];
+    p.background(0);
+    for (let pnt of a) {
+      pnt.draw(p);
+      pnt.next();
+      if (pnt.transparency === 0) {
+        deletes.push(pnt);
+      }
+    }
+
+    a = a.filter(pnt => !deletes.includes(pnt));
+
+    a.push(generatePoint());
+  }
 }
