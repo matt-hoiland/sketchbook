219915954d4b40dbb894d39aaea72348c5d2eb4f
Matt Hoiland <matthew.hoiland@vivint.com>
2019-06-13 15:38:21 -0600

    Convert 'mazes'

--- projects/conversion/src/app/app.component.ts
+++ projects/conversion/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import * as p5 from 'p5';
 
-import { LangtonsAnt } from '../sketches/langtons-ant';
+import { Mazes } from '../sketches/mazes';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +15,7 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.pInst = new p5(
-      LangtonsAnt.initSketch,
+      Mazes.initSketch,
       this.node.nativeElement
     );
   }
